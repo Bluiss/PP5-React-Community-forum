d2ec218b35e2f8bbe4da8c0d9bd17cd6a423dd9f
Harry Britton <harrydbritton@gmail.com>
2024-05-27 15:45:02 +0000

    feat: add postcreate function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row} from "react-bootstrap";
 import styles from "../styles/SearchBar.module.css";
 
 function SearchBar() {
